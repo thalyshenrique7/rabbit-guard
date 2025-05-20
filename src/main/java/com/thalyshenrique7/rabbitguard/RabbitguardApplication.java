b1753a57d58df6df5a912846f153a8101c5d5380
thalyshenrique7 <thalyshenriquedev@gmail.com>
2025-05-19 21:57:38 -0300

    initial commit

---
+++ src/main/java/com/thalyshenrique7/rabbitguard/RabbitguardApplication.java
@@ -0,0 +1,13 @@
+package com.thalyshenrique7.rabbitguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitguardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitguardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thalyshenrique7/rabbitguard/RabbitguardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thalyshenrique7.rabbitguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitguardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
